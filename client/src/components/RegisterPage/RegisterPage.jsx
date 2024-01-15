c6095455c2c889ea09c1003ec88ffc98870a7bb3
thapelomagqazana <tapsmcgzee8@gmail.com>
2024-01-15 07:05:32 +0200

    Added a Registration Page

--- client/src/components/RegisterPage/RegisterPage.jsx
+++ client/src/components/RegisterPage/RegisterPage.jsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 // import './AuthPage.css';
 
 const RegisterPage = () => {
+
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    console.log("Registration Data:", formData);
+  };
+
   return (
     <div className="auth-page">
       <header>
         <h1>Create Your Account</h1>
       </header>
       <main className="auth-main">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" required />
+          <input type="text" id="username" name="username" value={formData.username} onChange={handleChange} required />
 
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
+          <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required />
 
-          <label htmlFor="confirmPassword">Confirm Password:</label>
-          <input type="password" id="confirmPassword" name="confirmPassword" required />
+          {/* <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input type="password" id="confirmPassword" name="confirmPassword" required /> */}
 
           <button type="submit">Register</button>
         </form>
